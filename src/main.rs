14aafc79af6cf0e4612cc6b44d319be3c67e909d
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2021-03-13 00:24:10 +0100

    Decode hex even if has spaces or newline around

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::io::{self, Read, Write};
 
+fn trim(buffer: &[u8]) -> &[u8] {
+    let left = buffer.iter()
+        .take_while(|&&x| x == b' ' || x == b'\n').count();
+    let right = buffer.len() - buffer.iter().rev().take_while(|&&x| x == b' ' || x == b'\n').count();
+    &buffer[left..right]
+}
+
 fn hex_decode(buffer: &[u8]) -> Vec<u8> {
+    let buffer = trim(buffer);
     hex::decode(buffer).unwrap()
 }
 
@@ -14,4 +22,21 @@ fn main() {
     let decoded_string = hex_decode(buffer.as_bytes());
     stdout_lock.write(decoded_string.as_slice()).unwrap();
     stdout_lock.flush().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_trim() {
+        let s = b" 1234 \n";
+        assert_eq!(trim(s), b"1234");
+    }
+
+    #[test]
+    fn test_hex_decode() {
+        let s = b"6369616f\n";
+        assert_eq!(hex_decode(s), b"ciao");
+    }
 }
